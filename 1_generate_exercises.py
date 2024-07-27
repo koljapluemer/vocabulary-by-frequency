89862146e3f1ad6e949301dcf144fb614a14133c
blackbrokkoli <kastomat@gmail.com>
2024-07-27 21:12:03 +0300

    extract some more properties

--- 1_generate_exercises.py
+++ 1_generate_exercises.py
@@ -1,5 +1,6 @@
 import os
 import random
+import re
 
 
 OBSIDIAN_LEARN_PATH_DATA = "/home/b/MEGA/Obsidian/Zettelkasten/FrequentWordsLearning/Data"
@@ -20,12 +21,57 @@ def main():
         with open(os.path.join(OBSIDIAN_LEARN_PATH_DATA, f), 'r') as file:
             content = file.read()
             vocab = {}
+            valid_vocab_file = False
             # loop lines
-            for line in content.split("\n"):
+            lines = content.split("\n")
+            for index, line in enumerate(lines):
                 if '*target:*' in line:
-                    if len(line.split(":")) > 1:
-                        vocab['target'] = line.split(":")[1].replace("*", "").strip()
-                        print(vocab['target'])
+                    if len(line.strip().split(":")) > 1:
+                        target = line.split(":")[1].replace("*", "").strip()
+                        if target:
+                            # print(target)
+                            vocab['target'] = target
+                            valid_vocab_file = True
+                # same process for "native"
+                if '*native:*' in line:
+                    if len(line.strip().split(":")) > 1:
+                        source = line.split(":")[1].replace("*", "").strip()
+                        if source:
+                            print(source)
+                            vocab['native'] = source
+                # word_type
+                if '*word_type:*' in line:
+                    if len(line.strip().split(":")) > 1:
+                        word_type = line.split(":")[1].replace("*", "").strip()
+                        if word_type:
+                            vocab['word_type'] = word_type
+                # transliteration
+                if '*transliteration:*' in line:
+                    if len(line.strip().split(":")) > 1:
+                        transliteration = line.split(":")[1].replace("*", "").strip()
+                        if transliteration:
+                            vocab['transliteration'] = transliteration
+                # for the following one's, we're actually interested in the following lines
+                # search for following lines, until it starts with - or doesn't exist
+                # 1) "notes"
+                if '*notes:*' in line:
+                    notes = []
+                    for i in range(index+1, len(lines)):
+                        if lines[i].startswith("-"):
+                            break
+                        # use regex to kill everything like "- a)" or "-" or "- b)"
+                        clean_line = re.sub(r'-\s[a-z]\)', '', lines[i])
+                        clean_line = clean_line.replace("- ", "").strip()
+                        if clean_line:
+                            notes.append(clean_line)
+                    vocab['notes'] = notes
+                    print("notes:", notes)
+
+
+                
+            if not valid_vocab_file:
+                # print(f"File {f} is not a valid vocab file")
+                continue
 
 
 if __name__ == "__main__":
--- assets/vocab_template.md
+++ assets/vocab_template.md
@@ -1,6 +1,7 @@
 ---
 q-type: todo
 created-at: $DATE
+q-keywords: frequent-words
 auto-generate: true
 ---
 
